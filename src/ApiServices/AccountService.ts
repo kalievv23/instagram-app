506da59270061341a1a197308ed2a5b2f95b25f3
kalievv.23 <kaliev2004kg@gmail.com>
2024-03-14 15:36:44 +0600

    fixed import errors

--- src/ApiServices/AccountService.ts
+++ src/ApiServices/AccountService.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 import type { LoginModel, RegisterModel } from "../Domain/Models/index";
 import { MainURL, AccountEndpoints } from "../Domain/Constants/Urls/Urls";
 
